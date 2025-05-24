075ea2ec95e5b7c6b479f2ec30354585d22b8191
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-05-24 18:42:33 +0200

    Functionalities to tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import TodoTitle from './components/TodoTitle.jsx'
+import Todo from './components/Todo.jsx'
+import Popup from './components/Popup.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Hello World</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit dolor architecto nam nostrum nobis dignissimos sunt, quam itaque rerum, minus libero porro odio. Quisquam, accusantium omnis ipsum provident quaerat necessitatibus!</p>
+      <TodoTitle />
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+      <Popup />
     </>
   )
 }
---
+++ src/components/Popup.jsx
@@ -0,0 +1,16 @@
+function Popup() {
+    return (
+        <>
+            <div className="popup">
+                <span>Are you sure?</span>
+                <div className="popup__btns">
+                    <button className="popup__btn" onClick={() => { document.querySelector(".popup").style.display = "none"; document.querySelector(".backdrop").style.display = "none" }}>Confirm</button>
+                    <button className="popup__btn popup__btn--cancel">Cancel</button>
+                </div>
+            </div>
+            <div className="backdrop"></div>
+        </>
+    )
+}
+
+export default Popup
