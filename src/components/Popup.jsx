46afb14fb3ce05c1765a3bc77d711d6d0c6268e0
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-05-25 17:35:01 +0200

    Hiding backup and popup feature now is better and cleaner done

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,10 +1,15 @@
 function Popup({ question }) {
+    function hidingBackupPopup() {
+        document.querySelector('.popup').style.display = "none"
+        document.querySelector('.backdrop').style.display = "none"
+        console.log("Confirmed")
+    }
     return (
         <>
             <div className="popup">
                 <span>{question}</span>
                 <div className="popup__btns">
-                    <button className="popup__btn" onClick={() => { document.querySelector(".popup").style.display = "none"; document.querySelector(".backdrop").style.display = "none" }}>Confirm</button>
+                    <button className="popup__btn" onClick={hidingBackupPopup}>Confirm</button>
                     <button className="popup__btn popup__btn--cancel">Cancel</button>
                 </div>
             </div>
