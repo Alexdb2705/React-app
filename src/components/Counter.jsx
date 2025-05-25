0b50fab6a2fc9e0313349e0faf9af1e608c8a364
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-05-25 22:26:57 +0200

    Small fixing

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -9,7 +9,7 @@ function Counter() {
     return (
         <>
             <label htmlFor="#fontSize-change">Font-size of counter:</label>
-            <input type="number" id="fontSize-change" onChange={(event) => {document.querySelector("#counter-header").style.fontSize = event.target.value+"px"}} />
+            <input defaultValue={30} type="number" id="fontSize-change" onChange={(event) => {document.querySelector("#counter-header").style.fontSize = event.target.value+"px"}} />
             <div>
                 <h2 id="counter-header">Counter: {count}</h2>
                 <h2 className="user-header">User: {user.name}, {user.age} years.</h2>
@@ -19,27 +19,27 @@ function Counter() {
                         ...prevUser,
                         name: document.querySelector("#name-input").value
                     }))
-                }>Change name</button>
+                }>Change/set name</button>
                 <button onClick={() =>
                     setUser(prevUser => ({
                         ...prevUser,
                         age: prevUser.age + 1
                     }))
-                }>Increment age</button>
+                }>Increment age by 1</button>
                 <button onClick={() =>
                     setUser(prevUser => ({
                         ...prevUser,
                         age: prevUser.age - 1
                     }))
-                }>Decrement age</button>
+                }>Decrement age by 1</button>
                 <button onClick={() => {
                     setCount(prevCount => prevCount + 1);
                     setCount(prevCount => prevCount + 1)
-                }}>Increment 2 correct</button>
+                }}>Increment counter by 2 correct</button>
                 <button onClick={() => {
                     setCount(count + 1);
                     setCount(count + 1)
-                }}>Increment 2 incorrect</button>
+                }}>Increment counter by 2 incorrect</button>
                 <button onClick={() => setCount(count - 1)}>Decrement</button>
                 <button onClick={() => setCount(0)}>Reset</button>
             </div>
