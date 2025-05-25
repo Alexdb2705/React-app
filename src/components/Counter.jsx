1ba23f4f58b5138c0b7732e7a9f2248e86b08f46
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-05-25 20:58:30 +0200

    Counter implemented correctly making sure when a button has more actions inside its onClick everything works correctly

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,7 +5,14 @@ function Counter() {
     return (
         <>
             <h2 className="counter-header">Counter: {count}</h2>
-            <button onClick={() => setCount(count+1)}>Increment</button>
+            <button onClick={() => 
+                {setCount(prevCount => prevCount +1);
+                setCount(prevCount => prevCount +1)
+                }}>Increment 2 correct</button>
+            <button onClick={() => 
+                {setCount(count+1);
+                setCount(count+1)
+                }}>Increment 2 incorrect</button>
             <button onClick={() => setCount(count-1)}>Decrement</button>
             <button onClick={() => setCount(0)}>Reset</button>
         </>
