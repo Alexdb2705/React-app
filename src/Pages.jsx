a56bc68d15865284f4942124850398a620297e0a
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-05-26 20:20:17 +0200

    Changed form a to Link because using this component from react-router-dom, when a link is clicked the page is not reloaded again

--- src/Pages.jsx
+++ src/Pages.jsx
@@ -1,19 +1,26 @@
 import "./Pages.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Contact from "./pages/Contact.jsx"
+import Posts from "./pages/Posts.jsx"
 
 function Pages() {
     return (
         <Router>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/posts">Posts</Link>
+            </nav>
             <Routes>
-               <Route path="/" element={Home}/> 
-               <Route path="/about" element={About}/> 
-               <Route path="/contact" element={Contact}/>  
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/posts" element={<Posts />} />
             </Routes>
             <div>
-
             </div>
         </Router>
     )
