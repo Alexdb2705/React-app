56b6db3591d100b6989a17f9869efc53614d881f
Alejandro del Barrio Agudo <alejandrodelbarrioagudo@gmail.com>
2025-05-26 20:43:54 +0200

    Nav bar implemented

--- src/Pages.jsx
+++ src/Pages.jsx
@@ -1,19 +1,15 @@
 import "./Pages.css"
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Contact from "./pages/Contact.jsx"
 import Posts from "./pages/Posts.jsx"
+import Nav from "./components/Nav.jsx"
 
 function Pages() {
     return (
         <Router>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/posts">Posts</Link>
-            </nav>
+            <Nav />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
